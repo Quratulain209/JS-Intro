4ac2f2931a5efe834f0e12e700911829b8297d4a
Quratulain <aquratulain98@gmail.com>
2023-04-06 17:07:00 +0500

    Updated

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-prompt("Name: ", 'ainy')
+// prompt("Name: ", 'ainy')
+// alert ( name );
+// var name = "Mark";
+// name=235;
+// a=2;
+// b=3;
+// alert(a+b);
+// alert("2" + "2");
+// alert("2 plus 2 equals " + 2 + 2);
+
+ zakatPercentage = 0.025;
+ userInput = +prompt("Enter your Zakat value");
+ Result = zakatPercentage *  userInput;
+alert("Your Zakat value is "+ Result);
+
+               //  Day 01
+// alert("Thanks for your input!");
+
+// var name = "Mark";
+// name = "Ace";
+// alert(name)
+
+// var nationality;
+// nationality = "U.S.";
+// alert(nationality)
+
+// var thanx = "Thanks for your input!"
+// alert(thanx);
+
+// var weight = 150;
+// NEW = weight + 25;    
+// alert(NEW);
+
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+// alert(newNum)
